40b4127852fe0950a032a3cf0d33617b6c0b4764
tom-foley <foley.236@osu.edu>
2016-07-13 01:31:25 -0400

    Changed directory structure; Fixed tests directory; Fixed tail recursion issue; Added tests; Updated README

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# CalculatorTS
+#   CalculatorTS
 Calculator app ported from [my C ExpressionEvaluator](https://github.com/tom-foley/ExpressionEvaluator "ExpressionEvaluator") to TypeScript
+
+##  Compiling the Source
+To compile the TypeScript files into plain JS, run the following command in the root directory.
+```
+tsc -p .
+```
+
+##  Running the Tests
+To run the Calculator tests, run the following from a Node.JS environment in the root directory.
+```
+node test/test_calculator.js
+```
--- test/test_calculator.js
+++ test/test_calculator.js
@@ -1,20 +1,81 @@
-// import { Calculator, EvalResult } from '../src/calculator';
-// class CalculatorTest {
-//     successCount: number;
-//     testCount: number;
-//     calculator: Calculator.Calculator;
-//     constructor() {
-//         this.successCount = 0;
-//         this.testCount = 0;
-//     }
-//     public test(expression: string, expected: number) {
-//         this.testCount++;
-//         console.log("Expression:\t" + expression);
-//         let result: Calculator.EvalResult = this.calculator.calc(expression);
-//         console.log("Expected:\t" + expected);
-//         console.log("Result:\t\t\n" + result.result);
-//         this.successCount += (expected === result.result) ? 1 : 0;
-//     }
-// }
-// let ct = new CalculatorTest();
-// ct.test("1 + 3 * 2 + 1", 8);
+var Calculator = require('../lib/calculator');
+
+var CalcTest = (function () {
+    function CalcTest() {
+        this.ANSI_COLOR_RED = "\x1b[31m";
+        this.ANSI_COLOR_GREEN = "\x1b[32m";
+        this.ANSI_COLOR_RESET = "\x1b[0m";
+        this.testCount = 0;
+        this.successCount = 0;
+        this.calculator = new Calculator.Calculator();
+    }
+
+    CalcTest.prototype.test = function (expression, expected) {
+        this.testCount++;
+        console.log("Expression:\t" + expression);
+        var result = this.calculator.calc(expression);
+
+        console.log("Expected:\t" + expected);
+        console.log("Result:\t\t" + result.result);
+
+        if (result.error) {
+            console.log("Eval Result Error:\t" + result.errorMessage);
+        }
+
+        if (expected === result.result) {
+            this.successCount++;
+            console.log(this.ANSI_COLOR_GREEN + "***** Success *****" + this.ANSI_COLOR_RESET + "\n");
+        } else {
+
+            console.log(this.ANSI_COLOR_RED + "***** Error *****" + this.ANSI_COLOR_RESET + "\n");
+        }
+    }
+
+    return CalcTest;
+} ());
+
+var ct = new CalcTest();
+
+//  8
+ct.test("1 + 3 * 2 + 1", 8);
+
+//  109
+ct.test("(1 + 3 * (2 * (1 + 1) ^ 2) + 2) * 2 * 2 + 1", 109);
+
+//  -2
+ct.test("1 * - 2", -2);
+
+//  -1
+ct.test("2 / - 2", -1);
+
+//  -1
+ct.test("2 / (- 2)", -1);
+
+//  3
+ct.test("1 - - 2", 3);
+
+//  54
+ct.test("2 * (2 + 1) ^ 3", 54);
+
+//  26
+ct.test("3 * (2 * (1 + 1) ^ 2) + 2", 26);
+
+//  28
+ct.test("1 + 3 ^ (2 + 1)", 28);
+
+//  85
+ct.test("(1 + 3 * (2 * (2 + 1)) + 2) * 2 * 2 + 1", 85);
+
+//  16
+ct.test("(1 + (3 + 1) + 2) + 1 * 2 + 1 * 2 + (1 + 2 * 2)", 16);
+
+//  null --> Error due to illegal order of operations
+ct.test("2 */ 2", null);
+
+//  null --> Error due to missing closing parentheses
+ct.test("(1 + 3 * (2 * (1 + 1 ^ 2) + 2) * 2 * 2 + 1", null);
+
+//  null --> Error due to missing closing parentheses
+ct.test("2 / (- 2", null);
+
+console.log("Tests Passed:\t", ct.successCount + "/" + ct.testCount);
